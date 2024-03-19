093f05afcd46eb6bdd429954299ef84d7262ac9f
Kostiantyn <konstantinbelenko36@gmail.com>
2024-03-19 12:33:40 +0100

    vite optims + lazy loading + L + ratio

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import cssnano from "cssnano";
+import imagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   root: ".",
   build: {
     outDir: "dist",
     assetsDir: "assets",
-    minify: "esbuild",
+    minify: "terser",
   },
+  plugins: [imagemin(), cssnano()],
 });
